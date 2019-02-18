9b63c62459a44ba374253a11222dde9189b063f4
Elaina <elainaseki@gmail.com>
2019-02-18 04:20:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,8 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero_friends => []
