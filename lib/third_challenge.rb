3f64efe4c94283c8d582e7d20c41bc00831b8573
Elaina <elainaseki@gmail.com>
2019-02-18 04:25:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,8 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
@@ -31,7 +32,8 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15" 
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
