a46e4b9cb084b9da64449e17093dde078a908ab3
Elaina <elainaseki@gmail.com>
2019-02-18 04:28:23 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,6 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
+        {name: "Lord Montague", age: "53"},
+        {}
         #your hashes here!
       ]
    }, 
